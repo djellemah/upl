5a5ba85e7dd638a1e704905503b760b8df9ad989
John Anderson <panic@semiosix.com>
2020-02-11 18:31:40 -0500

    remove unused experiment

--- scratch/register_predicate.rb
+++
@@ -1,9 +0,0 @@
-def doit
-  fact = Upl::Term :person, :john, :anderson, Object.new
-  Upl.assertz fact
-  vs = Array Upl.query 'person(A,B,C)'
-  p vs
-  1000.times{Array Upl.query 'current_prolog_flag(K,V)'}
-  Upl.retract fact
-  Upl::Runtime.call Upl::Term :garbage_collect_atoms
-end
